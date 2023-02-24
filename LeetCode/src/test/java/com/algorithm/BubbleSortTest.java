f7aadc2d9b1ce18a4b42f92dc79bc8b9f86c5ac4
Yevgenii Krasnenkov <ykrasnenkov@corelogic.com>
2023-02-23 19:00:36 -0500

    tests iproved

--- LeetCode/src/test/java/com/algorithm/BubbleSortTest.java
+++ LeetCode/src/test/java/com/algorithm/BubbleSortTest.java
@@ -12,12 +12,15 @@ class BubbleSortTest {
         BubbleSort bubbleSort = new BubbleSort();
         int[] array = new int[]{123, 23, 45, 56, 78, 1, 5, 3, 2, 9};
         bubbleSort.apply(array);
-        boolean check = true;
+        Assertions.assertTrue(isSorted(array));
+    }
+
+    boolean isSorted(int[] array){
         for (int i = 1; i < array.length; i++) {
-            if (array[i - 1] > array[i]){
-                check = false;
+            if(array[i - 1] > array[i]) {
+                return false;
             }
         }
-        Assertions.assertTrue(check);
+        return true;
     }
 }
--- LeetCode/src/test/java/com/algorithm/QuickSortTest.java
+++ LeetCode/src/test/java/com/algorithm/QuickSortTest.java
@@ -12,12 +12,14 @@ class QuickSortTest {
         QuickSort quickSort = new QuickSort();
         int[] array = new int[]{123, 23, 45, 56, 78, 1, 5, 3, 2, 9};
         quickSort.apply(array, 0, array.length - 1);
-        boolean check = true;
+        Assertions.assertTrue(isSorted(array));
+    }
+    boolean isSorted(int[] array){
         for (int i = 1; i < array.length; i++) {
-            if (array[i - 1] > array[i]){
-                check = false;
+            if(array[i - 1] > array[i]) {
+                return false;
             }
         }
-        Assertions.assertTrue(check);
+        return true;
     }
 }
