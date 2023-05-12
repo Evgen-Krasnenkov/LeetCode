951cd0f8069ac966a6a2809e9bbc79b820d1a63f
Yevgenii Krasnenkov <ykrasnenkov@corelogic.com>
2023-05-12 12:40:35 -0400

    daily temperature with test

--- LeetCode/src/test/java/com/algorithm/DayTemperatureTest.java
+++ LeetCode/src/test/java/com/algorithm/DayTemperatureTest.java
@@ -13,11 +13,28 @@ class DayTemperatureTest {
         int[] exp2 = {1,1,1,0};
         int[] exp3 = {1,1,0};
         DayTemperature dayTemperature = new DayTemperature();
-        int[] actual2 = dayTemperature.dailyTemperatures(arr2);
-        Assertions.assertArrayEquals(exp2, actual2);
         int[] actual = dayTemperature.dailyTemperatures(arr);
         Assertions.assertArrayEquals(exp, actual);
+        int[] actual2 = dayTemperature.dailyTemperatures(arr2);
+        Assertions.assertArrayEquals(exp2, actual2);
         int[] actual3 = dayTemperature.dailyTemperatures(arr3);
+        Assertions.assertArrayEquals(exp3, actual3);
+    }
+
+    @Test
+    void givenNew_when_then() {
+        int[] arr = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] arr2 = {30,40,50,60};
+        int[] arr3 = {30,60,90};
+        int[] exp = {1,1,4,2,1,1,0,0};
+        int[] exp2 = {1,1,1,0};
+        int[] exp3 = {1,1,0};
+        DayTemperature dayTemperature = new DayTemperature();
+        int[] actual = dayTemperature.dailyAi(arr);
+        Assertions.assertArrayEquals(exp, actual);
+        int[] actual2 = dayTemperature.dailyAi(arr2);
+        Assertions.assertArrayEquals(exp2, actual2);
+        int[] actual3 = dayTemperature.dailyAi(arr3);
         Assertions.assertArrayEquals(exp3, actual3);
     }
 
