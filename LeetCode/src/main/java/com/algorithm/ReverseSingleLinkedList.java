4d1523effa1432cbef9841ab398d58c01672c402
Yevgenii Krasnenkov <ykrasnenkov@corelogic.com>
2023-08-03 23:47:52 -0400

    Reverse LinkedList with recursion

--- LeetCode/src/main/java/com/algorithm/ReverseSingleLinkedList.java
+++ LeetCode/src/main/java/com/algorithm/ReverseSingleLinkedList.java
@@ -1,25 +1,38 @@
 package com.algorithm;
+
 /**
- https://leetcode.com/problems/reverse-linked-list/
+ * https://leetcode.com/problems/reverse-linked-list/
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  **/
 public class ReverseSingleLinkedList {
+
     public ListNode reverseList(ListNode head) {
+        ListNode newTail = null;
+        if (head.next == null) {
+            return head;
+        }
+        newTail = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newTail;
+    }
+
+    public ListNode reverseListLoop(ListNode head) {
         if (head == null || head.next == null)
             return head;
         ListNode newHead = null;
         while (head != null) {
-            ListNode nextNode = head.next;
+            ListNode newList = head.next;
             head.next = newHead;
             newHead = head;
-            head = nextNode;
+            head = newList;
         }
         return newHead;
     }
