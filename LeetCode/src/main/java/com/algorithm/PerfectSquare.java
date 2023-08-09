40d57a3ec58942fdf68ed4bfdd245e814ba03455
Yevgenii Krasnenkov <ykrasnenkov@corelogic.com>
2023-08-09 00:50:02 -0400

    The least perfect squares in N

--- LeetCode/src/main/java/com/algorithm/PerfectSquare.java
+++ LeetCode/src/main/java/com/algorithm/PerfectSquare.java
@@ -3,13 +3,27 @@ package com.algorithm;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * https://leetcode.com/problems/perfect-squares/
  */
 public class PerfectSquare {
     public int numSquares(int n) {
+//        int[] dp = new int[n + 1];
+//        Arrays.fill(dp, n);
+//        dp[0] = 0;
+//        for (int i = 0; i <= n; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                int square = j * j;
+//                if (i - square < 0) {
+//                    break;
+//                } else {
+//                    dp[i] = Math.min(dp[i], 1 + dp[i - square]);
+//                }
+//            }
+//        }
+//        return dp[n];
+
         int[] dp = new int[n + 1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
@@ -18,7 +32,7 @@ public class PerfectSquare {
         for (int i = 1; i * i <= n; i++) {
             squares.add(i * i);
         }
-        for (int i = 1; i < squares.size(); i++) {
+        for (int i = 1; i <= n; i++) {
             for (Integer square : squares) {
                 if (i - square >= 0 && dp[i - square] != Integer.MAX_VALUE) {
                     dp[i] = Math.min(dp[i], dp[i - square] + 1);
