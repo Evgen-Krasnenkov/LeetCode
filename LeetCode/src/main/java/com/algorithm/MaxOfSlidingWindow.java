6fb9ef3f6a11867b7b136db134a6e1ee98df0ff3
Yevgenii Krasnenkov <ykrasnenkov@corelogic.com>
2023-08-11 00:05:13 -0400

    max of sliding window by deque

--- LeetCode/src/main/java/com/algorithm/MaxOfSlidingWindow.java
+++ LeetCode/src/main/java/com/algorithm/MaxOfSlidingWindow.java
@@ -1,7 +1,99 @@
 package com.algorithm;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class MaxOfSlidingWindow {
+
+    public int[] maxSlidingWindowDeque(int[] nums, int k) {
+        int[] result = new int[nums.length - k + 1];
+        Deque<Integer> window = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!window.isEmpty() && window.peek() < i - k + 1) {
+                window.poll();
+            }
+            while (!window.isEmpty() && nums[window.peekLast()] < nums[i]) {
+                window.pollLast();
+            }
+            window.offer(i);
+            if (i >= k - 1) {
+                result[i - k + 1] = nums[window.peek()];
+            }
+        }
+        return result;
+    }
+
+    public int[] maxSlidingWindowAlex(int[] nums, int k) {
+        int[] result = new int[nums.length - k + 1];
+        int maxIndex = -1;
+        int i = 0;
+        result[i] = Integer.MIN_VALUE;
+        for (int j = i; j < i + k; j++) {
+            if (result[i] < nums[j]) {
+                result[i] = nums[j];
+                maxIndex = j;
+            }
+        }
+        do {
+            i++;
+            if (maxIndex > i) {
+                if (nums[maxIndex] < nums[i + k - 1]) {
+                    result[i] = nums[i + k - 1];
+                    maxIndex = i + k - 1;
+                } else {
+                    result[i] = nums[maxIndex];
+                    maxIndex = maxIndex;
+                }
+            } else {
+                int show = i + k - 1;
+                System.out.println("nums length: " + nums.length + "! i + k - 1: " + show);
+                if (nums[maxIndex] < nums[i + k - 1]) {
+                    result[i] = nums[i + k - 1];
+                    maxIndex = i + k - 1;
+                } else {
+                    result[i] = Integer.MIN_VALUE;
+                    for (int j = i; j < i + k; j++) {
+                        if (result[i] < nums[j]) {
+                            result[i] = nums[j];
+                            maxIndex = j;
+                        }
+                    }
+                }
+            }
+        } while (i < nums.length - k);
+        return result;
+    }
+
     public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) {
+            return new int[0];
+        }
+
+        int n = nums.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+        leftMax[0] = nums[0];
+        rightMax[n - 1] = nums[n - 1];
+
+        // Compute left maxima for each block
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = (i % k == 0) ? nums[i] : Math.max(leftMax[i - 1], nums[i]);
+        }
+
+        // Compute right maxima for each block
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = (i % k == k - 1) ? nums[i] : Math.max(rightMax[i + 1], nums[i]);
+        }
+
+        int[] maxs = new int[n - k + 1];
+        for (int i = 0; i < n - k + 1; i++) {
+            maxs[i] = Math.max(rightMax[i], leftMax[i + k - 1]);
+        }
+
+        return maxs;
+    }
+
+    public int[] maxSlidingWindowLoop(int[] nums, int k) {
         int[] maxs = new int[nums.length - k + 1];
         for (int i = 0; i < nums.length - k + 1; i++) {
             int max = Integer.MIN_VALUE;
--- LeetCode/src/test/java/com/algorithm/MaxOfSlidingWindowTest.java
+++ LeetCode/src/test/java/com/algorithm/MaxOfSlidingWindowTest.java
@@ -3,9 +3,26 @@ package com.algorithm;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaxOfSlidingWindowTest {
+    @Test
+    public  void  givenArray_whenWindow3_then6MaxLoop(){
+        int[] numbers = {1,3,-1,-3,5,3,6,7};
+        MaxOfSlidingWindow maxOfSlidingWindow = new MaxOfSlidingWindow();
+        int[] window = maxOfSlidingWindow.maxSlidingWindowLoop(numbers, 3);
+        int[] maxs = {3, 3, 5, 5, 6, 7};
+        Assertions.assertArrayEquals(maxs, window);
+    }
     @Test
     public  void  givenArray_whenWindow3_then6Max(){
         int[] numbers = {1,3,-1,-3,5,3,6,7};
@@ -13,6 +30,37 @@ class MaxOfSlidingWindowTest {
         int[] window = maxOfSlidingWindow.maxSlidingWindow(numbers, 3);
         int[] maxs = {3, 3, 5, 5, 6, 7};
         Assertions.assertArrayEquals(maxs, window);
+    }
+
+    @Test
+    public  void  givenArray_whenWindow3_then6MaxDeque(){
+        int[] numbers = {1,3,-1,-3,5,3,6,7};
+        MaxOfSlidingWindow maxOfSlidingWindow = new MaxOfSlidingWindow();
+        int[] window = maxOfSlidingWindow.maxSlidingWindowDeque(numbers, 3);
+        int[] maxs = {3, 3, 5, 5, 6, 7};
+        Assertions.assertArrayEquals(maxs, window);
+    }
+
+    @Test
+    public  void  givenArray_whenWindow3_then6MaxLeftSum(){
+        int[] numbers = {-7,-8,7,5,7,1,6,0};
+        MaxOfSlidingWindow maxOfSlidingWindow = new MaxOfSlidingWindow();
+        int[] window = maxOfSlidingWindow.maxSlidingWindowAlex(numbers, 4);
+        int[] maxs = {7,7,7,7,7};
+        Assertions.assertArrayEquals(maxs, window);
+    }
+
+    @Test
+    public  void  givenArray_whenWindow3_then6MaxLeftSumLarge() throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get("src/main/java/com/algorithm/numbers"));
+        int[] number = lines.stream()
+                .flatMap(line -> Arrays.stream(line.split(",")))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int[] numbers = {1,3,-1,-3,5,3,6,7};
+        MaxOfSlidingWindow maxOfSlidingWindow = new MaxOfSlidingWindow();
+        int[] window = maxOfSlidingWindow.maxSlidingWindowAlex(numbers, 3);
+        Assertions.assertArrayEquals(new int[]{3, 3, 5, 5, 6, 7}, window);
     }
 
 }
