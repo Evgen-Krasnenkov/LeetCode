55b85cc203562057f29aa9cb6c1bd17f2f571b02
Yevgenii Krasnenkov <ykrasnenkov@corelogic.com>
2023-08-11 17:16:41 -0400

    priority queue for Max Gap

--- LeetCode/src/main/java/com/algorithm/MaxGap.java
+++ LeetCode/src/main/java/com/algorithm/MaxGap.java
@@ -1,7 +1,56 @@
 package com.algorithm;
 
+import java.util.PriorityQueue;
+
 public class MaxGap {
-    public int maximumGap(int[] nums) {
-        return -1;
+
+    class Bucket {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
     }
+
+    public int maximumGap(int[] nums) {
+        int n = nums.length;
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(nums[i], max);
+            min = Math.min(nums[i], min);
+        }
+        System.out.println("Length: " + n);
+        int bucketSize = Math.max(1, (max - min) / (n - 1));
+        Bucket[] buckets = new Bucket[(max - min) / bucketSize + 1];
+        for (int num : nums) {
+            int index = (num - min) / bucketSize;
+            if (buckets[index] == null) {
+                buckets[index] = new Bucket();
+            }
+            buckets[index].min = Math.min(buckets[index].min, num);
+            buckets[index].max = Math.max(buckets[index].max, num);
+        }
+        int maxGap = 0;
+        int previousMax = min;
+        for (Bucket bucket : buckets) {
+            if (bucket != null) {
+                maxGap = Math.max(maxGap, bucket.min - previousMax);
+                previousMax = bucket.max;
+            }
+        }
+        return maxGap;
+    }
+
+    public int maximumGapPriorityQueue(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums){
+            pq.add(num);
+        }
+        int max_diff = 0;
+        while (pq.size() > 1) {
+            Integer poll = pq.poll();
+            Integer peek = pq.peek();
+            max_diff = Math.max(max_diff, Math.abs(poll - peek));
+        }
+        return max_diff;
+    }
+
 }
--- LeetCode/src/test/java/com/algorithm/MaxGapTest.java
+++ LeetCode/src/test/java/com/algorithm/MaxGapTest.java
@@ -9,9 +9,22 @@ class MaxGapTest {
     @Test
     public void givenArray4_whenMaxGap_thenOk(){
         MaxGap maxGap = new MaxGap();
-        int[] numbers = {3,6,9,1};
-        int gap = maxGap.maximumGap(numbers);
+        int[] nums = {3,6,9,1};
+        int gap = maxGap.maximumGap(nums);
         Assertions.assertEquals(3, gap);
+        nums = new int[]{1, 1, 1, 1, 1, 5, 5, 5, 5, 5};
+        gap = maxGap.maximumGap(nums);
+        Assertions.assertEquals(4, gap);
     }
 
+    @Test
+    public void givenArray4_whenMaxGap_thenQueueOk(){
+        MaxGap maxGap = new MaxGap();
+        int[] nums = {3,6,9,1};
+        int gap = maxGap.maximumGapPriorityQueue(nums);
+        Assertions.assertEquals(3, gap);
+        nums = new int[]{1, 1, 1, 1, 1, 5, 5, 5, 5, 5};
+        gap = maxGap.maximumGapPriorityQueue(nums);
+        Assertions.assertEquals(4, gap);
+    }
 }
